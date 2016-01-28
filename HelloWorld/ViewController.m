29213844201fb7bea9c662090eaa9098d9aba535
admin <you@example.com>
2016-01-28 18:10:05 +0800

    zzr commit

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //zzr 2016-2-8
 }
 
 - (void)didReceiveMemoryWarning {
